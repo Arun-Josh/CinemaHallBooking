7ea7dd65087805a95b299a52805bc0cf3d5c49fa
Dragon <astralarunjosh@gmail.com>
2019-06-27 10:53:38 +0530

    Initialised to Move the Model to DB

--- src/Main.java
+++ src/Main.java
@@ -9,37 +9,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         Display display = new Display();
         Utils utils = new Utils();
-//        LinkedList<Shows> shows = new LinkedList<>();
-        Manipulate manipulate = new Manipulate();
 
-//        //Fare
-//        HashMap<String, Double> showFare = new HashMap<>();
-//        showFare.put("PLATINUM",300d);
-//        showFare.put("GOLD",200d);
-//        showFare.put("SILVER",100d);
-
-//        int SCREEN_NO;
-//
-//        SCREEN_NO = 1;
-//        HashMap<String, Integer[][]> screen = new HashMap();
-//        screen.putAll(manipulate.addScreen(SCREEN_NO));
-//        shows.add(new Shows(100,SCREEN_NO, screen,"Titanic","06:10","2:50",showFare));
-//        SCREEN_NO = 2;
-//        screen = new HashMap();
-//        screen.putAll(manipulate.addScreen(SCREEN_NO));
-//        shows.add(new Shows(101,SCREEN_NO, screen,"Titanic","06:10","2:50",showFare));
-//        SCREEN_NO = 1;
-//        screen = new HashMap();
-//        screen.putAll(manipulate.addScreen(SCREEN_NO));
-//        shows.add(new Shows(102,SCREEN_NO, screen,"Edge Of Tomorrow","14:10","2:25",showFare));
-//        SCREEN_NO = 2;
-//        screen = new HashMap();
-//        screen.putAll(manipulate.addScreen(SCREEN_NO));
-//        shows.add(new Shows(103,SCREEN_NO, screen,"Elysium","20:10","3:15",showFare));
-//        SCREEN_NO = 3;
-//        screen = new HashMap();
-//        screen.putAll(manipulate.addScreen(SCREEN_NO));
-//        shows.add(new Shows(104,SCREEN_NO, screen,"Battle Ship","08:10","1:35",showFare));
         while(choice!=-1){
             display.listOptions();
             choice = display.getChoice(0,6);
@@ -118,7 +88,7 @@ public class Main {
             }
         }
 
-        System.out.println("*-*-*-*-*-*-*-* Thank you for using our Service :) *-*-*-*-*-*-*-*");
+        System.out.println("\n*-*-*-*-*-*-*-* Thank you for using our Service :) *-*-*-*-*-*-*-*\n");
 
 
     }
